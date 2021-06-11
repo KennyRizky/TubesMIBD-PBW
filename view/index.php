d23ff3a0832f8fba9a1ebb55f49d00b49efb852f
putra443 <putra443@gmail.com>
2021-06-11 21:46:34 +0700

    Home Page
    baru bikin sesuai sama mockup, cmn masih kurang simetris dikit2

---
+++ view/index.php
@@ -0,0 +1,36 @@
+<div id = "home1">
+    <h1 class = "header1">WHY LEARN WITH US?</h1>
+    <div class = "header1Content">
+        <h2>A+</h2>
+        <h2 >$</h2>
+        <h2><i class="fa fa-clock-o"></i></h2>
+        <h2><i class="fa fa-check"></i></h2>
+    </div>
+    <div class = "header1Content1">
+        <p>Improve your Grades</p>
+        <p>Affordable</p>
+        <p>Quick Learning</p>
+        <p>Get Approved Certificate</p>
+    </div>
+</div>
+<div id = "home2">
+    <h1 class = "header1">Follow Courses to Maximize your Potential</h1>
+    <div class = "header1Content">
+        <h2><i class="fa fa-search"></i></h2>
+        <h2><i class="fa fa-check-circle"></i></h2>
+        <h2><i class="fa fa-certificate"></i></h2>
+        <h2><i class="fa fa-globe"></i></h2>
+    </div>
+    <div class = "header1Content1">
+        <p>Select Course</p>
+        <p>Finish Course</p>
+        <p>Get Certificate</p>
+        <p>Show Off your Certificate</p>
+    </div>
+</div>
+<div id = "home3">
+    <h1 class = "header1">START LEARNING NOW!</h1>
+    <div id ="startBtn">
+    <input type ="button" name="get-started" value="Get Started" id="Btn">
+    </div>
+</div>
