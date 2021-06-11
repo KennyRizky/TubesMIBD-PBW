9268909c14d3a18d220f462db245380f13f66a08
Christopher William <christ.william.bill@gmail.com>
2021-06-11 07:17:07 +0700

    Test
    test

--- view/layout/layout.php
+++ view/layout/layout.php
@@ -6,9 +6,12 @@
 <body>
 	<div id="header">
 		<a id="logo" href="index">Study Hub</a>
+		<a href="index">Home</a>
+		<a href="index">Courses</a>
+		<a href="index">About</a>
+
+
 	</div>
-
-
 	<?php echo $content; ?>
 
 </body>
