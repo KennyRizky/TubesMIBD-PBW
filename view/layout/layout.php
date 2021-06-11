5288a9474e02bf06e36a569d49527b3dca36173d
Christopher William <christ.william.bill@gmail.com>
2021-06-11 07:38:42 +0700

    Header
    tambahin css

--- view/layout/layout.php
+++ view/layout/layout.php
@@ -2,13 +2,29 @@
 <html>
 <head>
 	<link rel="stylesheet" href="view/css/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 	<div id="header">
+
 		<a id="logo" href="index">Study Hub</a>
-		<a href="index">Home</a>
-		<a href="index">Courses</a>
-		<a href="index">About</a>
+		<div id="nav">
+
+			<a class="navbutton" href="index">Home</a>
+			<a class="navbutton" href="index">Courses</a>
+			<a class="navbutton"href="index">About</a>
+
+			<div id="searchbar">
+				<input id="logosearch" type="text" placeholder="Search..">
+				<i class="fa fa-search"></i>
+			</div>
+
+
+		</div>
+		<div id="account">
+			<a class="navbutton" href="index">Sign In</a>
+			<a class="navbutton" href="index">Register</a>
+		</div>
 	</div>
 	<?php echo $content; ?>
 
