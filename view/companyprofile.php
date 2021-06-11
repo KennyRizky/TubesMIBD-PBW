359912b8592e09927bc1a18d654888cd456a812e
Christopher William <christ.william.bill@gmail.com>
2021-06-11 20:14:30 +0700

    Company Profile

--- view/companyprofile.php
+++ view/companyprofile.php
@@ -1,24 +1,73 @@
 <div id="slideshow">
-    SLIDE SHOW
+    <div class="slides fade">
+        <img src="slide1.jpg" style="width:100%">
+        <div class="slidestext">Caption Text</div>
+    </div>
+
+    <div class="slides fade">
+        <img src="slide2.jpg" style="width:100%">
+        <div class="slidestext">Caption Text</div>
+    </div>
+
+    <div class="slides fade">
+        <img src="slide3.jpg" style="width:100%">
+        <div class="slidestext">Caption Text</div>
+    </div>
 </div>
 
 <div id="info">
     <div id="contact">
-        <h3>About Us</h3>
-            <p>Study Hub was founded during the covid-19 pandemic. We know the importance of education, and the pandemic is a threat to young generation's education. We are here to provide everybody's need with knowledge and education.</p>
+        <h1>Our Story</h1>
+            <p class="visimisitext">
+            Study Hub was founded during the covid-19 pandemic. 
+            We know the importance of education, and the pandemic is a threat to 
+            young generation's education. We are here to provide everybody's 
+            need with knowledge and education.
+            </p>
+
+            <p class="visimisitext">
+            Backed by more than <b>5,000 certified teachers</b>,
+            hundreds of available courses, and the <b>best certificates</b>,
+            Study Hub now has more than <b>100,000 learners</b> around the globe.
+            </p>
 
         <h3>Contact Us</h3>
-        <i class="fa fa-instagram fa-2x icon"></i>
-        <i class="fa fa-facebook-square fa-2x icon"></i>
-        <i class="fa fa-envelope fa-2x icon"></i>
+            <i class="fa fa-instagram fa-2x icon"></i>
+            <i class="fa fa-facebook-square fa-2x icon"></i>
+            <i class="fa fa-envelope fa-2x icon"></i>
 
     </div>
 
     <div id="visimisi">
-        <h2>Our Vision</h2>
-        <p>Our vision is to make people smart. Making people smart is our vision.</p>
-        <h2>Our Mission</h2>
-        <p>Our mission is to make people smart. Making people smart is our mission</p>
+        <h1>Our Vision</h1>
+        <p class="visimisitext">
+        Learning is the source of human progress.
+
+        It has the power to transform our world
+        from illness to health,
+        from poverty to prosperity,
+        from conflict to peace.
+
+        It has the power to transform our lives
+        for ourselves,
+        for our families,
+        for our communities.
+        </p>
+
+        <p class="visimisitext">
+        No matter who we are or where we are,
+        learning empowers us to change and grow
+        and redefine what’s possible.
+        That’s why access to the best learning is a right, not a privilege.
+        </p>
+
+        <h1>Our Mission</h1>
+        <p class="visimisitext">
+        We foster an enthusiastic, creative community of learners prepared to continue 
+        their intellectual, emotional, and physical development.
+        We will have a transformative impact on society through continual innovation 
+        in education, research, creativity, and entrepreneurship
+        </p>
 
     </div>
 </div>
