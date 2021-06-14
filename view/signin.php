f7956d5d64549fac2cafe0f8d3337f2887a4b4d1
putra443 <putra443@gmail.com>
2021-06-14 15:50:30 +0700

    benerin checker

--- view/signin.php
+++ view/signin.php
@@ -8,8 +8,8 @@
         <h1 class="JudulForm">Sign In</h1>
         <form action="">
         <div id= "formSignIn">       
-            <input type="text" id="uName" name="username" onkeyup="vUser()" placeholder="username"><img src="css/checker.png" id="checker1"><br>
-            <input type="password" id="pass" name="password" onkeyup="vPass()" placeholder="password"><img src="css/checker.png" id="checker2"><br>
+            <input type="text" id="uName" name="username" onkeyup="vUser()" placeholder="username"><img src="view/css/checker.png" id="checker1"><br>
+            <input type="password" id="pass" name="password" onkeyup="vPass()" placeholder="password"><img src="view/css/checker.png" id="checker2"><br>
         </div>
         <div id ="startBtn">
             <input type="submit" value="Sign In" id="butSignIn">
