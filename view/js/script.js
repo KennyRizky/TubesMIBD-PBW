06f080fcfbcf513da7df6bb198874de8d95cd5c6
Christopher William <christ.william.bill@gmail.com>
2021-06-15 11:10:56 +0700

    validasi

--- view/js/script.js
+++ view/js/script.js
@@ -15,38 +15,90 @@ function showSlides() {
     setTimeout(showSlides, 4000); 
 }
 
-function vUser(){
-    let valueUser = document.getElementById('uName');
-    if(valueUser.value.length >= 8){
-        document.getElementById('checker1').style.display = "inline";
-    }
-    else{
-        document.getElementById('checker1').style.display = "none";
-    }
-}
+// function vUser(){
+//     let valueUser = document.getElementById('uName');
+//     if(valueUser.value.length >= 8){
+//         document.getElementById('checker1').style.display = "inline";
+//     }
+//     else{
+//         document.getElementById('checker1').style.display = "none";
+//     }
+// }
     
-function vPass(){
-    let valuePassword = document.getElementById('pass');
-    if(valuePassword.value.length >= 8){
-    document.getElementById('checker2').style.display = "inline";
-    }
-    else{
-    document.getElementById('checker2').style.display = "none";
+// function vPass(){
+//     let valuePassword = document.getElementById('pass');
+//     if(valuePassword.value.length >= 8){
+//     document.getElementById('checker2').style.display = "inline";
+//     }
+//     else{
+//     document.getElementById('checker2').style.display = "none";
+//     }
+// }
+
+// function vEmail(){
+//     let valueEmail = document.getElementById('email');
+//     let mailFormat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+//     if(valueEmail.value.match(mailFormat)){
+//         document.getElementById('checker0').style.display = "inline";
+//         console.log('email benar');
+//     }
+//     else{
+//         document.getElementById('checker0').style.display = "none";
+//         console.log('email salah');
+//     }
+// }
+
+/*
+-------------
+VALIDASI FORM
+-------------
+*/
+
+let Vuser = false;
+let Vpass = false;
+
+//menampilkan gambar centang untuk username dan mengecek validity username
+function checkVuser() {
+    let x = document.getElementById("uName").value;
+    console.log(x);
+    if(x.length >= 8){
+        console.log('berhasil');
+        document.getElementById("centang1").style.display = "block";
+        Vuser = true;
+    }else{
+        document.getElementById("centang1").style.display = "none";
+        Vuser = false;
     }
 }
 
-function vEmail(){
-    let valueEmail = document.getElementById('email');
-    let mailFormat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
-    if(valueEmail.value.match(mailFormat)){
-        document.getElementById('checker0').style.display = "inline";
-        console.log('email benar');
+//menampilkan gambar centang untuk password dan mengecek validity password
+function checkVpass() {
+    var x = document.getElementById("pass").value;
+    if(x.length >= 8){
+        document.getElementById("centang2").style.display = "block";
+        Vpass = true;
+    }else{
+        document.getElementById("centang2").style.display = "none";
+        Vpass = false;
     }
-    else{
-        document.getElementById('checker0').style.display = "none";
-        console.log('email salah');
+}	
+
+//mengecek validity form, menampilkan konfirmasi registrasi dan tombol register
+function checkForValidity(){
+    if(Vpass === true && Vuser === true){
+        isValid = true;
+        document.getElementById("startBtn").style.display = "flex";
+    }else{
+        isValid = false;
+        alert("masih ada form yang belum valid!");
+        document.getElementById("startBtn").style.display = "none";
+
     }
 }
 
-
+//menghilangkan konfirmasi registrasi dan tombol register jika user mengganti isi form
+//user harus validasi ulang jika mengganti isi form
+function hide(){
+    document.getElementById("startBtn").style.display = "none";
+}
 
--- view/register.php
+++ view/register.php
@@ -7,17 +7,17 @@
     <div id="kotakForm">
         <h1 class="JudulForm">Register</h1>
         <form action="submitRegister" method="POST">
-        <div id= "formSignIn">
-            <input type="text" id="email" name="email" placeholder="email"><br>
-            <input type="text" id="uName" name="username" placeholder="username"><br>
-            <input type="password" id="pass" name="password" placeholder="password"><br>
-            <input type="text" id="alamat" name="alamat" placeholder="alamat"><br>
-            <input type="date" id="birthDate", name="birthDate"><br>
-            
-        </div>
-        <div id ="startBtn">
-            <input type="submit" value="register" id="butSignIn">
-        </div>
+            <div id= "formSignIn">
+                <input type="text" id="email" name="email" placeholder="email">
+                <input type="text" id="uName" name="username" placeholder="username" oninput="checkVuser();"><img src = "view/css/check.png" id="centang1">
+                <input type="password" id="pass" name="password" placeholder="password" oninput="checkVpass(); hide();"><img src = "view/css/check.png" id="centang2">
+                <input type="text" id="alamat" name="alamat" placeholder="alamat"><br>
+                <input type="date" id="birthDate", name="birthDate"><br>   
+            </div>
+
+            <div id ="startBtn">
+                <input type="submit" value="register" id="butSignIn">
+            </div>
         </form>
         <p>Already have an Account? <a href="signin">Sign in</a></p>
         <p>Are you a teacher? <a href="registerTeacher">Register as Teacher</a></p>
