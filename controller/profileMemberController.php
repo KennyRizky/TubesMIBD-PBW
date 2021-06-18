fb6e4a0dd596453a4bf2a0ab8882882ddba3e71a
Christopher William <christ.william.bill@gmail.com>
2021-06-18 14:57:51 +0700

    profile jadi

--- controller/profileMemberController.php
+++ controller/profileMemberController.php
@@ -1,6 +1,8 @@
 <?php
 require_once "controller/services/mysqlDB.php";
 require_once "controller/services/view.php";
+require_once "model/member.php";
+
 
 class profileMemberController{
     protected $db;
@@ -10,10 +12,23 @@ class profileMemberController{
     }
 
     public function view_profileMember(){
-        return View::createView('profileMember.php',[]);
+        $result = $this->getProfile();
+        return View::createView('profileMember.php',['result'=>$result]);
     }
     
+    public function getProfile(){
+        session_start();
+        $nama = $_SESSION['username'];
 
+        $query = "SELECT * FROM Member WHERE nama = '$nama'";
+        $query_result = $this->db->executeSelectQuery($query);
+        
+        $result = [];
+        foreach($query_result as $key => $value){
+            $result[] = new Member($value['IdM'], $value['email'], $value['nama'], $value['pass'], $value['alamat'],$value['tgllahir'],$value['wallet']);
+        }
+        return $result;
+    }
     
 }
 
--- view/profileMember.php
+++ view/profileMember.php
@@ -1,9 +1,26 @@
+
 <div id="kotakProfile">
     <div>
-        <p>Username :</p>
-        <p>Password :</p>
-        <p>Role :</p>
-        <p>Date of Birth :</p>
-        <p>Wallet :</p>
+        <?php
+		foreach ($result as $key => $row) {
+			echo "<p>Username: ".$row->getUsername()."</p>";
+            echo "<p>Password: ".$row->getPassword()."</p>";
+            echo "<p>Role: Member";
+			echo "<p>Email: ".$row->getEmail()."</p>";
+			echo "<p>Date of Birth: ".$row->getBirthDate()."</p>";
+			echo "<p>Wallet: ".$row->getWallet()."</p>";
+
+        }
+        // <p>Username :</p>
+        // <p>Password :</p>
+        // <p>Role :</p>
+        // <p>Date of Birth :</p>
+        // <p>Wallet :</p>
+        ?>
+
+        <a href="index">Back</a>
+        <a href="editProfile">Edit Profile</a>
+        <a href="buyCredit">Buy Credit</a>
+
     </div>
 </div>
